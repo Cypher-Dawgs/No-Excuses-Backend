3003ef06b526cc4e9647c2cfd865a7e42d97acfc
Paarth <paarth.jain2020@vitstudent.ac.in>
2022-08-06 18:06:26 +0530

    inital-commit

---
+++ api/routes/admin.routes.js
@@ -0,0 +1,26 @@
+const { adminController } = require("../controllers");
+
+const router = require("express").Router();
+const { isAuth } = require("../middlewares/auth");
+const {
+  validateUserSignUp,
+  userValidation,
+  validateUserSignIn,
+} = require("../middlewares/validation/user");
+
+router.post("/create-admin", validateUserSignUp, userValidation, (req, res) => {
+  adminController.createUser(req, res);
+});
+router.post(
+  "/sign-in-admin",
+  validateUserSignIn,
+  userValidation,
+  (req, res) => {
+    adminController.userSignIn(req, res);
+  }
+);
+router.post("/sign-out", isAuth, (req, res) => {
+  adminController.signOut(req, res);
+});
+
+module.exports = router;
